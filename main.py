1ce0cb5d5de6aa9838bd71aa13ee56378816fd13
Ali <alibenhadjyahia6@mail.com>
2026-04-19 20:07:08 +0200

    clean : 2 notebooks seulement + main.py uvicorn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def main():
-    print("Hello from projet-info-2as2!")
-
+import uvicorn
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("src.api.api:app", host="0.0.0.0", port=8000, reload=True)
--- src/api/api.py
+++ src/api/api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from src.api.routers.backtest_router import router as backtest_router
+from src.api.core.config import add_cors
 
 app = FastAPI(title="DAX Backtest API", version="1.0.0")
 
+add_cors(app)
 app.include_router(backtest_router)
 
 
